b05df4eb4a856fa73ad572212592023d6a2394a0
milliephanillie <me@philiparudy.com>
2024-01-23 03:19:45 -0500

    fix link title

--- src/Admin/Menu/SQMGMT_AdminMenu.php
+++ src/Admin/Menu/SQMGMT_AdminMenu.php
@@ -271,7 +271,7 @@ class SQMGMT_AdminMenu
 					<div class="row-actions">
 						<span class="id">ID: '.esc_attr($product['id']).'</span>
 						<span class="edit">
-						<a href="'.esc_attr($product['edit_link']).'" aria-label="Edit “Health Gummies”">Edit</a>
+						<a href="'.esc_attr($product['edit_link']).'" aria-label="Edit “' . esc_attr($product['name']) . '”">Edit</a>
 						</span>
 					</div>
 
